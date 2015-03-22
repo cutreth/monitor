4bfb159ae27c36fd56f863926c13d079b3289469
KY <kikot.world@gmail.com>
2015-03-22 17:32:46 -0500

    CSS static files

--- cutreth/settings.py
+++ cutreth/settings.py
@@ -83,7 +83,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
