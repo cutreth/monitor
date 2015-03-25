c109548f4b9d0bc1af49c989c4d3ec9c7ac20ab9
KY <kikot.world@gmail.com>
2015-03-25 06:01:11 -0500

    Config lookup/graph ordering fixes

--- monitor/views.py
+++ monitor/views.py
@@ -192,7 +192,7 @@ def api(request):
     key = stringFromPost(request, 'key')
     if (key == 'beer') or (key == 'test'):
 
-        active_config = Config.objects.get(pk=2) #Get config 1
+        active_config = Config.objects.filter()[:1].get()
         active_beer = active_config.beer #Get active beer
 
         read = Reading(beer=active_beer) #Create reading record
@@ -312,7 +312,7 @@ def chart(request, cur_beer=None):
     all_beers = Beer.objects.all()
 
     if cur_beer is None:
-        active_config = Config.objects.get(pk=2)
+        active_config = Config.objects.filter()[:1].get()
         active_beer = active_config.beer
     else:
         active_beer = Beer.objects.get(pk=cur_beer)
@@ -408,7 +408,7 @@ def graph(request,cur_beer=None):
     import mpld3 
     
     if cur_beer is None:
-        active_config = Config.objects.get(pk=2)
+        active_config = Config.objects.filter()[:1].get()
         active_beer = active_config.beer
     else:
         active_beer = Beer.objects.get(pk=cur_beer)
@@ -430,7 +430,7 @@ def createFig(vers, active_beer):
     from mpld3 import plugins
     import matplotlib.dates as mpld
 
-    active_readings = Reading.objects.filter(beer=active_beer)
+    active_readings = Reading.objects.filter(beer=active_beer).order_by('instant_actual')
     instant_data = [mpld.date2num(n.instant_actual) for n in active_readings]
     
     x_count = len(active_readings)
