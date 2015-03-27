93327d63d1db76d884d791c4ab94a561790854fe
KY <kikot.world@gmail.com>
2015-03-27 17:50:52 -0500

    URL updates

--- monitor/urls.py
+++ monitor/urls.py
@@ -5,11 +5,14 @@ from monitor import views
 urlpatterns = patterns('',
     url(r'^api/$', views.api, name='api'),
     url(r'^commands/$', views.commands, name='commands'),
-    url(r'^commands/(?P<command_char>.+)/$', views.send_command, name='send_command'),
+    url(r'^commands/(?P<command_char>.+)/$', views.send_command),
     url(r'^chart/$', views.chart, name='chart_default'),
-    url(r'^chart/(?P<cur_beer>\d+)/$', views.chart, name='chart_beer'),
+    url(r'^chart/(?P<cur_beer>\d+)/$', views.chart),
     url(r'^graph/$', views.graph, name='graph_default'),
-    url(r'^graph/(?P<cur_beer>\d+)/$', views.graph, name='graph_beer'),
+    url(r'^graph/(?P<cur_beer>\d+)/$', views.graph),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^dashboard/update/$', views.dashboard_update, name='dashboard_update'),
+    url('',views.dashboard)
 )
+
+#http://pythex.org/
