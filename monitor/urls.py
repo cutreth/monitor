86e2e49fce5a257395bf43aee7cdb605010619b5
Bcorwin <bscorwin@gmail.com>
2015-03-27 16:07:53 -0500

    Fixed url name for dashboard_update

--- monitor/urls.py
+++ monitor/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
     url(r'^chart/(?P<cur_beer>\d+)/$', views.chart, name='chart_beer'),
     url(r'^graph/$', views.graph, name='graph_default'),
     url(r'^graph/(?P<cur_beer>\d+)/$', views.graph, name='graph_beer'),
-    url(r'^dashboard/$', views.dashboard, name='dashboard_default'),
-    url(r'^dashboard/update/$', views.dashboard_update, name='dashboard_default'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^dashboard/update/$', views.dashboard_update, name='dashboard_update'),
 )
