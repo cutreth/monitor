872af1b8f4d28689de73ac69c8943bb4af998cc3
Bcorwin <bscorwin@gmail.com>
2015-03-27 12:54:57 -0500

    Made suggested changes.

--- monitor/urls.py
+++ monitor/urls.py
@@ -11,5 +11,4 @@ urlpatterns = patterns('',
     url(r'^graph/$', views.graph, name='graph_default'),
     url(r'^graph/(?P<cur_beer>\d+)/$', views.graph, name='graph_beer'),
     url(r'^dashboard/$', views.dashboard, name='dashboard_default'),
-    url(r'^dashboard/(?P<cur_beer>\d+)/$', views.dashboard, name='graph_beer'),
 )
