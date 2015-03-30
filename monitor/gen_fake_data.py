4b863b21b89dc45ad1f986777734c9d68501be87
KY <kikot.world@gmail.com>
2015-03-30 18:00:42 -0500

    Tweaking data generator

--- monitor/gen_fake_data.py
+++ monitor/gen_fake_data.py
@@ -1,19 +1,17 @@
-from monitor.models import Beer, Reading
+from monitor.models import Reading
+from monitor.views import getActiveBeer
 import random
 
-def gen_fake_data(beer_name, n):
-    #Create new beer
-    b = Beer(beer_text = beer_name)
-    b.save()
+def gen_fake_data(n, beer=None):
+    if beer == None:
+        beer = getActiveBeer()
     
     #Add readings to beer
     for i in range(n):
-        r = Reading(beer = b,
+        r = Reading(beer = beer,
                     light_amb = random.normalvariate(100, 20),
                     pres_beer = random.normalvariate(150, 20),
                     temp_amb = random.normalvariate(70, 2),
                     temp_beer = random.normalvariate(70, 2)
                 )
         r.save()
-
-gen_fake_data("Tons of Readings", 5000)
