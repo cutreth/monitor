0d484763897a7bae1558c29272e1f9c040b98dab
Bcorwin <bscorwin@gmail.com>
2015-03-30 22:30:53 -0500

    Initial 2nd axis work

--- monitor/views.py
+++ monitor/views.py
@@ -460,7 +460,11 @@ def chart(request, cur_beer = None):
     readings = getReadings(active_beer)
     plot_data = []
     for r in readings:
-        add = [r.instant_actual.isoformat(), r.get_temp_amb(), 'undefined', 'undefined', r.get_temp_beer(), 'undefined', 'undefined']
+        add = [r.instant_actual.isoformat(),
+                r.get_temp_amb(), 'undefined', 'undefined',
+                r.get_temp_beer(), 'undefined', 'undefined',
+                r.get_light_amb(), 'undefined', 'undefined'
+            ]
         plot_data.append(add)
     
     data = {
