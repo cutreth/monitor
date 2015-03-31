2d3bfe50648e3e0525ba386998c1fe1a4c494b7b
KY <kikot.world@gmail.com>
2015-03-31 17:39:43 -0500

    New getLastReading function

--- monitor/views.py
+++ monitor/views.py
@@ -324,6 +324,12 @@ def getReadings(active_beer):
     '''Return all readings for active_beer ordered by instant_actual'''
     active_readings = Reading.objects.filter(beer=active_beer).order_by('-instant_actual_iso')
     return active_readings
+    
+def getLastReading(active_beer):
+    '''Returns the most recent reading for active_beer if it exists'''
+    last_read = None
+    last_read = getReadings(active_beer)[:1].get()
+    return last_read
 
 def createDF(active_beer):
     '''Return a DF of reading/archive data, ordered by instant'''
