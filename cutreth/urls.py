2251b595e31f7ac3db147c27e5eaf3f9db34305b
KY <kikot.world@gmail.com>
2015-03-31 20:31:04 -0500

    Password protecting 'commands' within /admin/

--- cutreth/urls.py
+++ cutreth/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cutreth.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^admin/commands/$', 'monitor.views.commands', name='commands'),
+    url(r'^admin/commands/(?P<command_char>.+)/$', 'monitor.views.send_command'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^monitor/', include('monitor.urls')),
 )
