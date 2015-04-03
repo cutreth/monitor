922c82367ed5d7a8f4a00adf4b3bb51d13d1ffd0
KY <kikot.world@gmail.com>
2015-04-03 09:51:03 -0500

    Combining data generation functions

--- monitor/data_loader.py
+++ monitor/data_loader.py
@@ -1,5 +1,6 @@
-from monitor.models import Reading
+from monitor.models import Beer, Reading
 from monitor.views import getActiveBeer
+from datetime import datetime, timedelta
 import random
 
 def gen_fake_data(n, beer=None):
@@ -15,3 +16,31 @@ def gen_fake_data(n, beer=None):
                     temp_beer = random.normalvariate(70, 2)
                 )
         r.save()
+
+def gen_blank_beer():
+    now = datetime.now()
+    name = "Blank Beer"
+    b1 = Beer.objects.filter(beer_text = name)
+    b1.delete()
+    b1 = Beer(beer_text = name, brew_date = now)
+    b1.save()
+
+def gen_two_weeks():
+    now = datetime.now()
+    name = "Two Weeks"
+    b2 = Beer.objects.filter(beer_text = name)
+    Reading.objects.filter(beer=b2).delete()
+    b2 = Beer(beer_text = name, brew_date = now)
+    b2.save()
+    
+    cur_datetime = now - timedelta(days = 14)
+    while(cur_datetime <= now):
+        r = Reading(beer = b2,
+                instant_override = cur_datetime,
+                light_amb = random.normalvariate(100, 10),
+                pres_beer = random.normalvariate(50, 5),
+                temp_amb = random.normalvariate(70, 2),
+                temp_beer = random.normalvariate(72, 1)
+            )
+        r.save()
+        cur_datetime = cur_datetime + timedelta(seconds = 60*15)
