514d091aa05e65d9feb408cb9559f610f04c4ce3
KY <kikot.world@gmail.com>
2015-04-13 08:24:00 -0500

    Data loader tweaks

--- monitor/data_loader.py
+++ monitor/data_loader.py
@@ -1,15 +1,15 @@
+import monitor.api as api
+import monitor.do as do
 from monitor.models import Beer, Reading
-from monitor.do import getActiveBeer, getActiveConfig
-from monitor.api import BoundsErrorCheck
-from datetime import datetime, timedelta
-import pytz
-import random
+
+from datetime import timedelta
+from random import normalvariate
 
 def gen_fake_data(n, beer=None):
     if beer == None:
-        beer = getActiveBeer()
-    active_config = getActiveConfig()
-    
+        beer = do.getActiveBeer()
+    active_config = do.getActiveConfig()
+
     #Add readings to beer
     for i in range(n):
         r = Reading(beer = beer,
@@ -19,7 +19,7 @@ def gen_fake_data(n, beer=None):
                     temp_beer = normalvariate(70, 2)
                 )
         r.save()
-        error = BoundsErrorCheck(active_config, r)
+        api.BoundsErrorCheck(active_config, r)
 
 def create_or_modify(beer_name,date):
     beer = Beer.objects.filter(beer_text=beer_name)
@@ -29,28 +29,28 @@ def create_or_modify(beer_name,date):
         beer = Beer(beer_text=beer_name, brew_date=date)
         beer.save()
     return beer
-        
+
 def gen_blank_beer():
-    now = datetime.utcnow().replace(tzinfo=pytz.utc)
+    now = do.nowInUtc()
     name = 'Blank Beer'
     beer = create_or_modify(name,now)
-    
+
     readings = Reading.objects.filter(beer=beer)
     readings.delete()
 
 def gen_two_weeks():
-    now = datetime.utcnow().replace(tzinfo=pytz.utc)
+    now = do.nowInUtc()
     name = "Two Weeks"
-    beer = create_or_modify(name,now)    
-    
+    beer = create_or_modify(name,now)
+
     cur_datetime = now - timedelta(days = 14)
     while(cur_datetime <= now):
         r = Reading(beer = beer,
                 instant_override = cur_datetime,
-                light_amb = random.normalvariate(100, 10),
-                pres_beer = random.normalvariate(50, 5),
-                temp_amb = random.normalvariate(70, 2),
-                temp_beer = random.normalvariate(72, 1)
+                light_amb = normalvariate(100, 10),
+                pres_beer = normalvariate(50, 5),
+                temp_amb = normalvariate(70, 2),
+                temp_beer = normalvariate(72, 1)
             )
         r.save()
         cur_datetime = cur_datetime + timedelta(seconds = 60*15)
