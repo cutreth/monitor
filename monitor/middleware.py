1123a4a2ce53e63ce5d2b262e222c5916b29d9c4
Bcorwin <bscorwin@gmail.com>
2015-04-16 14:49:48 -0500

    Updated documentation for middleware.py

--- monitor/middleware.py
+++ monitor/middleware.py
@@ -63,65 +63,61 @@ Example call = Response
 
 ----------------------------------------------------------------
 
-F - Force a log or attempt to rerun main loop (depends on state)
+code=F - Force a log or attempt to rerun main loop (depends on state)
 
-send2middleware("f") = ('Success', 'Forcing log...')
-
-----------------------------------------------------------------
-
-M=n - Set the log period to n minutes
-
-send2middleware("m=1")	= ('Success', 'Changed now minLog=1')	#Returns new minLog value
-send2middleware("m")	= ('Success', 'Log frequency=15')		#Returns current minLog value
-send2middleware("m=")	= ('Fail', 'Number needed. minLog=15')	#Returns current minLog value
+send2middleware("code=f", testMode = True) = ('Success', 'Forcing log...')
 
 ----------------------------------------------------------------
 
-R=var_name - Get the averages that are currently in memory for var_name
+code=m&freq=n - Set the log period to n minutes
 
-send2middleware("r=temp_amb")	= ('Success', 'temp_amb:75.8')
-send2middleware("r")		= ('Success', "{'temp_unit': 'F', 'inst")	#Attempts to return all vars; will come back truncated
-send2middleware("r=light_beer")	= ('Fail', 'light_beer is not valid.')
-send2middleware("r=")		= ('Fail', ' is not valid.')
-
-----------------------------------------------------------------
-
-O - Turns off data collection and logging
-
-send2middleware("O")	= ('Success', 'Data collection off.')
-send2middleware("O")	= ('Fail', 'Was already off.')
-----------------------------------------------------------------
-
-L - Turns off data collection and logging
-
-send2middleware("L")	= ('Success', 'Data collection on.')
-send2middleware("L")	= ('Fail', 'Was already on.')
+send2middleware("code=m&freq=1", testMode = True)   = ('Success', 'Changed now minLog=1')
+send2middleware("code=m", testMode = True)          = ('Success', 'Log frequency=15')
+send2middleware("code=m&freq=", testMode = True)    = ('Success', 'Log frequency=1')
+send2middleware("code=m&freq=", testMode = True)    = ('Success', 'Log frequency=1')
 
 ----------------------------------------------------------------
 
-E - Turns off remote logging, continues to add logs to queue
+code=r&var=var_name - Get the averages that are currently in memory for var_name
 
-send2middleware("E")	= ('Success', 'Remote logging off.')
-send2middleware("E")	= ('Fail', 'Remote logging was already off.')
-----------------------------------------------------------------
-
-D - Turns on remote logging
-
-send2middleware("D")	= ('Success', 'Remote logging on.')
-send2middleware("D")	= ('Fail', 'Remote logging was already on.')
-
-----------------------------------------------------------------
-!!DEFUNCT!!
-C - Turns off server permanently, will require access to the server to turn it back on
-
-send2middleware("c", True)	= ('Success', 'Cancelled.')
+send2middleware("code=r&var=temp_amb", testMode = True)     = ('Success', 'temp_amb:75.8')
+send2middleware("code=r", testMode = True)		            = ('Success', "{'temp_unit': 'F', 'inst")	#Attempts to return all vars; will come back truncated
+send2middleware("code=r&var=light_beer", testMode = True)   = ('Fail', 'light_beer is not valid.')
+send2middleware("code=r&var=", testMode = True)             = ('Fail', ' is not valid.')
 
 ----------------------------------------------------------------
 
-A=var_name[min,max] - Turns on audible alerts from the middleware for var_name when out of the min-max range
+code=c&dir=direction - Turns on/off data collection and logging
 
-send2middleware("a=temp_beer[70,80]", testMode = True)  = ('Success', 'Alert for temp_beer set to [70, 80]')
-send2middleware("a=off[0,0]", testMode = True)          = ('Success', 'Alerts turned off')
+send2middleware("code=c&dir=off", testMode = True)      = ('Success', 'Data collection off.')
+send2middleware("code=c&dir=off", testMode = True)      = ('Fail', 'Was already off.')
+send2middleware("code=c&dir=on", testMode = True)       = ('Success', 'Data collection on.')
+send2middleware("code=c&dir=on", testMode = True)       = ('Fail', 'Was already on.')
+send2middleware("code=c&dir=toggle", testMode = True)   = ('Success', 'Data collection on.')
+send2middleware("code=c&dir=toggle", testMode = True)   = ('Success', 'Data collection off.')
+send2middleware("code=c&dir=", testMode = True)         = ('Success', 'Data collection off.') #Status report
+send2middleware("code=c", testMode = True)              = ('Success', 'Data collection off.') #Status report
+
+----------------------------------------------------------------
+
+code=L&dir=direction - Turns on/off remote logging, continues to add logs to queue
+
+send2middleware("code=L&dir=off", testMode = True)      = ('Success', 'Remote logging off.')
+send2middleware("code=L&dir=off", testMode = True)      = ('Fail', 'Was already off.')
+send2middleware("code=L&dir=on", testMode = True)       = ('Success', 'Remote logging on.')
+send2middleware("code=L&dir=on", testMode = True)       = ('Fail', 'Was already on.')
+send2middleware("code=L&dir=toggle", testMode = True)   = ('Success', 'Remote logging on.')
+send2middleware("code=L&dir=toggle", testMode = True)   = ('Success', 'Remote logging off.')
+send2middleware("code=L&dir=", testMode = True)         = ('Success', 'Remote logging off.') #Status report
+send2middleware("code=L", testMode = True)              = ('Success', 'Remote logging off.') #Status report
+
+----------------------------------------------------------------
+
+code=a&var=var_name&min=min_val&max=max_val - Turns on audible alerts from the middleware for var_name when out of the min_val-max_val range
+!!Do not exclude var, min, or max (unless sending 'off' as var) as it may lock up  server (needs to be tested in production to know for sure)!!
+
+send2middleware("code=a&var=temp_beer&max=80&min=70", testMode = True)  = ('Success', 'Alert for temp_beer set to [70, 80]')
+send2middleware("code=a&var=off", testMode = True)                      =( 'Success', 'Alerts turned off')
 
 ----------------------------------------------------------------
 '''
